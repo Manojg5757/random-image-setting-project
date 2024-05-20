ce46412195792289429f971c51d7c58b664ec94c
manojvsp <gmanojece2021@gmail.com>
2024-05-20 12:49:42 +0530

    random image setting

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-let time = "false"
+const image = document.getElementById("image")
+const set = document.getElementById("set")
+const remove = document.getElementById("remove")
+const container = document.getElementById("container")
+const randomNumber = Math.floor(Math.random()*4)
+const doc = ['1','2','3','4']
+console.log(randomNumber)
+function handleSet(){
+    const randomNumber = Math.floor(Math.random()*4)
+    image.setAttribute('src',`/${doc[randomNumber]}.jpg`)
+    container.style.display = 'block'
+   
+   
+}
 
-typeof(time) == "boolean" ? console.log("Boolean yeah") : console.log("not")
+function handleRemove(){
+    image.removeAttribute('src')
+    container.style.display = 'none'
+}
